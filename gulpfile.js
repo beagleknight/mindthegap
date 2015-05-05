c3adb43757a91f14c0288084af028f32abc794d6
ultrayoshi <david.morcillo@gmail.com>
2015-05-05 21:09:55 +0200

    Basic Phaser skeleton

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp           = require('gulp'),
+    jade           = require('gulp-jade'),
+    connect        = require('gulp-connect'),
+    jshint         = require('gulp-jshint'),
+    concat         = require('gulp-concat'),
+    LIB_FILES      = ['bower_components/phaser/build/phaser.min.js'],
+    SCRIPT_FILES   = ['src/js/**/*.js'],
+    STYLE_FILES    = ['src/css/**/*.css'],
+    TEMPLATE_FILES = ['src/templates/**/*.jade'],
+    ASSET_FILES    = ['src/assets/**/*'];
+
+gulp.task('lint', function() {
+    gulp.src(SCRIPT_FILES)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('libs', function() {
+    gulp.src(LIB_FILES)
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('scripts', ['lint'], function() {
+    gulp.src(SCRIPT_FILES)
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('styles', function() {
+    gulp.src(STYLE_FILES)
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('templates', function () {
+    gulp.src(TEMPLATE_FILES)
+        .pipe(jade())
+        .pipe(gulp.dest('public'))
+        .pipe(connect.reload());
+});
+
+gulp.task('assets', function () {
+    gulp.src(ASSET_FILES)
+        .pipe(gulp.dest('public/assets'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch(SCRIPT_FILES, ['scripts']);
+    gulp.watch(STYLE_FILES, ['styles']);
+    gulp.watch(TEMPLATE_FILES, ['templates']);
+    gulp.watch(ASSET_FILES, ['assets']);
+});
+
+gulp.task('server', function () {
+    connect.server({
+        root: 'public',
+        livereload: true
+    });
+});
+
+gulp.task('build', ['templates', 'scripts', 'styles', 'libs', 'assets']);
+
+gulp.task('default', ['watch', 'server']);
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+
+    new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+        preload: function () {
+
+        },
+        create: function () {
+
+        },
+        update: function () {
+
+        }
+    });
+}());
